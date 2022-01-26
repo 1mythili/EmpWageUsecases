74a78eb68081a5a74bd5cd56cf2df5bdcc0c9c17
1mythili <jaimyaa98@gmail.com>
2022-01-26 02:55:05 -0800

    first program

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using FellowshipUsecase;
+
+Console.WriteLine("Hello, World!");
+//UC1_EmployeeAttendance.GetAttendance();
+//UC2_EmpWage.EmpWage();
+//UC3_PartTimeEmpWage.PartTimeEmpWage();
+UC4_EmpWageUsingSwitch.EmpWageUsingSwitch();
+//UC5_CalculateWagesFor20DaysInMonth.CalculateWagesFor20DaysInMonth();
+//UC6_CalculateWageTill100HrsOr20IsReached.CalculateWageTill100HrsOr20IsReached();
+//UC7_ComputeEmpWageUsingClassMethod.ComputeEmpWage();
+//UC8_ComputeEmpWageForMultipleCompanies.ComputeEmpWage("TCS",20,2,10);
